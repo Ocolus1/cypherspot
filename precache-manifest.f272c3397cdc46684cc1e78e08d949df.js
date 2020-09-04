9ec731d4526e0d5649b4686b63ef1c92899ee45d
Cypherspot <awolesiboluwatife@gmail.com>
2020-09-04 07:53:50 -0700

    Updates

---
+++ precache-manifest.f272c3397cdc46684cc1e78e08d949df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9af89b96cf17f8529b0c69a1f93c83e",
+    "url": "/cypherspot/index.html"
+  },
+  {
+    "revision": "8e98040653da56cc3d02",
+    "url": "/cypherspot/static/js/2.ed1219b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cypherspot/static/js/2.ed1219b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0becec3d9d715219810",
+    "url": "/cypherspot/static/js/main.2837f9da.chunk.js"
+  },
+  {
+    "revision": "d364f57385a208309955",
+    "url": "/cypherspot/static/js/runtime-main.09a9f47b.js"
+  },
+  {
+    "revision": "1f43e3065b1929c48e028b0b2647b768",
+    "url": "/cypherspot/static/media/Paul.1f43e306.jpg"
+  },
+  {
+    "revision": "7aa2e856922a8c2450d0c25cf949821c",
+    "url": "/cypherspot/static/media/paul_malta.7aa2e856.pdf"
+  }
+]);
